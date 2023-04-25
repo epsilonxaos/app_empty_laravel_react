38bb5f87fe127649345254020add997d3059af3c
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-04-25 10:04:41 -0600

    Cargando el env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import laravel from "laravel-vite-plugin";
+import fs from "fs";
 
-export default defineConfig({
-    plugins: [
-        react(),
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            react(),
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.js"],
+                refresh: true,
+            }),
+        ],
+        define: {
+            APP_ENV: {
+                APP_URL: env.APP_URL,
+            },
+        },
+        // server: {
+        //     host: env.SERVER_HOST,
+        //     port: 5173,
+        //     https: {
+        //         key: fs.readFileSync(env.SERVER_HTTPS_KEY),
+        //         cert: fs.readFileSync(env.SERVER_HTTPS_CERT),
+        //     },
+        // },
+    };
 });
