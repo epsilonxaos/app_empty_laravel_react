3e06e2d63c56a3695fadd58c2203272381d3eca5
Jesus Glez <jesus.gonzalez@merida.gob.mx>
2024-05-17 11:15:04 -0600

    fix(Web): :rotating_light: Importacion de dependencia

--- resources/js/react/App.jsx
+++ resources/js/react/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import { useLocation } from "react-router-dom";
 
 const initialArgs = {
     loading: true,
