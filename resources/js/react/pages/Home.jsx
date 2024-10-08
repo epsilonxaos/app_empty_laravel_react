4fef648d645253839e3d4d6c2f5c89df12b3d633
Epsilonxaos <angelotr12@hotmail.com>
2024-10-08 13:24:42 -0600

    refactor(Config): :wrench: Configurando Eslint y Prettier

--- resources/js/react/pages/Home.jsx
+++ resources/js/react/pages/Home.jsx
@@ -1,3 +1,9 @@
+import Footer from '@/components/Footer'
+
 export default function Home() {
-    return <>Inicio de proyecto</>;
+	return (
+		<div>
+			<Footer />
+		</div>
+	)
 }
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,48 @@
-import { defineConfig, loadEnv } from "vite";
-import laravel from "laravel-vite-plugin";
-import { readFileSync } from "fs";
-import react from "@vitejs/plugin-react";
+import { readFileSync } from 'fs'
+import laravel from 'laravel-vite-plugin'
+import { defineConfig, loadEnv } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
-    const host = env.SERVER_HOST;
-    const isProduction = mode === "production";
+import react from '@vitejs/plugin-react'
 
-    return {
-        server: isProduction
-            ? false
-            : {
-                  host,
-                  port: 5174,
-                  hmr: { host },
-                  https: {
-                      key: readFileSync(env.SERVER_HTTPS_KEY),
-                      cert: readFileSync(env.SERVER_HTTPS_CERT),
-                  },
-              },
-        plugins: [
-            laravel({
-                input: [
-                    "resources/css/app.css",
-                    "resources/css/panel/app.css",
-                    "resources/js/app.js",
-                    "resources/js/react/index.jsx",
-                    "resources/js/panel/index.js",
-                    "resources/js/panel/trumbowygInit.js",
-                ],
-                refresh: true,
-            }),
-            react(),
-        ],
-    };
-});
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	const host = env.SERVER_HOST
+	const isProduction = mode === 'production'
+
+	return {
+		server: isProduction
+			? false
+			: {
+					host,
+					port: 5174,
+					hmr: { host },
+					https: {
+						key: readFileSync(env.SERVER_HTTPS_KEY),
+						cert: readFileSync(env.SERVER_HTTPS_CERT),
+					},
+				},
+
+		plugins: [
+			laravel({
+				input: [
+					'resources/css/app.css',
+					'resources/css/panel/app.css',
+					'resources/js/app.js',
+					'resources/js/react/index.jsx',
+					'resources/js/panel/index.js',
+					'resources/js/panel/trumbowygInit.js',
+				],
+				refresh: true,
+			}),
+			react(),
+			tsconfigPaths(),
+		],
+
+		resolve: {
+			alias: {
+				'@': 'resources/js/react',
+			},
+		},
+	}
+})
