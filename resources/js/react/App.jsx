9e52d3508f29017e113475a3f67deea66fab2701
Jesus Glez <jesus.gonzalez@merida.gob.mx>
2025-01-24 16:01:57 -0600

    feat(Web): :sparkles: Reorganización del componente Footer y actualización de configuraciones de ESLint

--- resources/js/react/App.jsx
+++ resources/js/react/App.jsx
@@ -1,47 +1,53 @@
-import { useReducer } from "react";
-import { AnimatePresence } from "framer-motion";
+import Footer from '@modules/Footer'
+import { AnimatePresence } from 'framer-motion'
 
-import ScrollToTop from "./components/ScrollToTop";
-import PageTransition from "./components/PageTransition";
-import Loading from "./components/Loading";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import PageNotFound from "./pages/PageNotFound";
-import { useLocation } from "react-router-dom";
+import { useReducer } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+
+import Loading from './components/Loading'
+import PageTransition from './components/PageTransition'
+import ScrollToTop from './components/ScrollToTop'
+import Header from './modules/Header'
+import Home from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
 
 const initialArgs = {
-    loading: true,
-};
+	loading: true,
+}
 
-const reducer = (prev, next) => ({ ...prev, ...next });
+const reducer = (prev, next) => ({ ...prev, ...next })
 
 export default function App() {
-    const location = useLocation();
-    const [state, dispatch] = useReducer(reducer, initialArgs);
+	const location = useLocation()
+	const [state, dispatch] = useReducer(reducer, initialArgs)
 
-    if (state.loading) return <Loading />;
+	if (state.loading) return <Loading />
 
-    return (
-        <>
-            <Header />
-            <AnimatePresence mode="wait">
-                <ScrollToTop />
+	return (
+		<>
+			<Header />
+			<AnimatePresence mode='wait'>
+				<ScrollToTop />
 
-                <Routes location={location} key={location.pathname}>
-                    <Route
-                        index
-                        element={
-                            <PageTransition>
-                                <Home />
-                            </PageTransition>
-                        }
-                    />
+				<Routes
+					location={location}
+					key={location.pathname}>
+					<Route
+						index
+						element={
+							<PageTransition>
+								<Home />
+							</PageTransition>
+						}
+					/>
 
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
-            </AnimatePresence>
-            <Footer />
-        </>
-    );
+					<Route
+						path='*'
+						element={<PageNotFound />}
+					/>
+				</Routes>
+			</AnimatePresence>
+			<Footer />
+		</>
+	)
 }
---
+++ resources/js/react/modules/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+		<footer className='w-full px-4 py-5'>
+			<ul className='mx-auto flex w-full max-w-[1600px] flex-col items-center justify-between md:flex-row'>
+				<li>Project name</li>
+				<li>
+					Diseño y desarrollo x <span className='font-bold'>MadebyPartners®</span>
+				</li>
+				<li>Políticas de privacidad</li>
+			</ul>
+		</footer>
+	)
+}
+
+export default Footer
